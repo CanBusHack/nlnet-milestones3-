fc9a044aa449d0c48e0af02e32a1c1769387a341
Isaac Tepper <isaac@canbushack.com>
2024-04-30 17:56:46 +0200

    Call the ESP OTA functions

--- components/omni_ota/ota.c
+++ components/omni_ota/ota.c
@@ -19,16 +19,70 @@
 /** Logging tag (omni_ota) */
 static const char tag[] = "omni_ota";
 
+/** Global OTA handle */
+static esp_ota_handle_t ota_handle;
+
+/** Flag for currently running OTA */
+static int ota_started = 0;
+
+/** OTA update initializer */
+static int ota_begin(void) {
+    if (ota_started) {
+        ESP_LOGI(tag, "abort current OTA!");
+        esp_ota_abort(ota_handle);
+        ota_started = 0;
+    }
+    ESP_LOGI(tag, "write firmware begin");
+
+    const esp_partition_t* update_partition = esp_ota_get_next_update_partition(NULL);
+    if (!update_partition) {
+        ESP_LOGE(tag, "no update partition available!");
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    esp_err_t err = esp_ota_begin(update_partition, OTA_SIZE_UNKNOWN, &ota_handle);
+    if (err == ESP_OK) {
+        ota_started = 1;
+        return 0;
+    } else {
+        ESP_LOGE(tag, "begin error: %d", err);
+    }
+    return err;
+}
+
 /** OTA update callback */
-static int ota_update(void* address, size_t size) {
-    ESP_LOGI(tag, "write firmware at %p, size %zu", address, size);
-    return 0;
+static int ota_update(const void* data, size_t size, uint32_t offset) {
+    ESP_LOGI(tag, "write firmware at 0x%08lx, size %zu", offset, size);
+    if (ota_started) {
+        esp_err_t err = esp_ota_write_with_offset(ota_handle, data, size, offset);
+        if (err == ESP_OK) {
+            return 0;
+        } else {
+            ESP_LOGE(tag, "write_with_offset error: %d", err);
+        }
+        return err;
+    } else {
+        ESP_LOGE(tag, "OTA not started yet!");
+        return ESP_ERR_INVALID_ARG;
+    }
 }
 
 /** OTA update finalizer */
 static int ota_end(void) {
     ESP_LOGI(tag, "write firmware finished");
-    return 0;
+    if (ota_started) {
+        esp_err_t err = esp_ota_end(ota_handle);
+        ota_started = 0;
+        if (err == ESP_OK) {
+            return 0;
+        } else {
+            ESP_LOGE(tag, "end error: %d", err);
+        }
+        return err;
+    } else {
+        ESP_LOGE(tag, "OTA not started yet!");
+        return ESP_ERR_INVALID_ARG;
+    }
 }
 
 #ifdef CONFIG_OMNITRIX_ENABLE_BLE
@@ -51,19 +105,26 @@ static int gatt_svc_access(uint16_t conn_handle, uint16_t attr_handle, struct bl
 
             static uint8_t buffer[256];
             uint16_t om_len = OS_MBUF_PKTLEN(ctxt->om);
-            if (om_len > sizeof(uintptr_t) && om_len < sizeof(buffer)) {
+            if (om_len > sizeof(uint32_t) && om_len <= sizeof(buffer)) {
                 uint16_t om_len_2;
                 int rc = ble_hs_mbuf_to_flat(ctxt->om, buffer, sizeof(buffer), &om_len_2);
                 assert(rc == 0);
+                assert(om_len == om_len_2);
 
-                uintptr_t ptr = 0;
-                for (int i = 0; i < sizeof(uintptr_t); i++) {
+                uint32_t ptr = 0;
+                for (int i = 0; i < sizeof(ptr); i++) {
                     ptr = (ptr << 8) | buffer[i];
                 }
-                if (ptr == UINTPTR_MAX) {
+                switch (ptr) {
+                case 0xFFFFFFFE:
+                    rc = ota_begin();
+                    break;
+                case 0xFFFFFFFF:
                     rc = ota_end();
-                } else {
-                    rc = ota_update((void*)ptr, om_len - sizeof(uintptr_t));
+                    break;
+                default:
+                    rc = ota_update(buffer + sizeof(ptr), om_len - sizeof(ptr), ptr);
+                    break;
                 }
                 if (rc != 0) {
                     ESP_LOGE(tag, "error writing OTA: %d", rc);
