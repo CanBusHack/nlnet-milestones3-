51baedbc20b27310159a2c655e209ce4ab1214ec
Isaac Tepper <isaac@canbushack.com>
2024-08-07 17:48:28 -0400

    Add tests for reading with extended address and no pad

--- components/omni_hello/test.c
+++ components/omni_hello/test.c
@@ -95,9 +95,9 @@ static void test_read_unmatched(void) {
     assert(read_frame == 0xDEADBEEF);
 }
 
-static void test_read_multi(void) {
+static void test_read_multi_normal(void) {
     uint8_t read_data[256] = { 0 };
-    size_t read_size;
+    size_t read_size = 0;
 
     void get_next_event(struct isotp_event* evt) {
         static const struct isotp_event events[] = {
@@ -174,6 +174,258 @@ static void test_read_multi(void) {
     uint8_t expected_data[] = "\x00\x00\x07\xE8\x49\x02\x01"
                               "ABCDEFGHIJKLMNOPQ";
     assert(!memcmp(read_data, expected_data, 24));
+}
+
+static void test_read_multi_extended(void) {
+    uint8_t read_data[256] = { 0 };
+    size_t read_size = 0;
+
+    void get_next_event(struct isotp_event* evt) {
+        static const struct isotp_event events[] = {
+            {
+                .type = EVENT_RECONFIGURE_PAIRS,
+                .pairs = {
+                    .size = 12,
+                    .data = { 0x60, 0x00, 0x07, 0xE0, 0xFE, 0x42, 0x60, 0x00, 0x07, 0xE8, 0xEF, 0x24 },
+                },
+            },
+            {
+                .type = EVENT_INCOMING_CAN,
+                .can = {
+                    .id = 0x7e8,
+                    .dlc = 8,
+                    .data = { 0xEF, 0x10, 0x14, 0x49, 0x02, 0x01, 0x41, 0x42 },
+                    .frame = (void*)(uintptr_t)0xDEADBEEF,
+                },
+            },
+            {
+                .type = EVENT_INCOMING_CAN,
+                .can = {
+                    .id = 0x7e8,
+                    .dlc = 8,
+                    .data = { 0xEF, 0x21, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48 },
+                    .frame = (void*)(uintptr_t)0xDEADBEEF,
+                },
+            },
+            {
+                .type = EVENT_INCOMING_CAN,
+                .can = {
+                    .id = 0x7e8,
+                    .dlc = 8,
+                    .data = { 0xEF, 0x22, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E },
+                    .frame = (void*)(uintptr_t)0xDEADBEEF,
+                },
+            },
+            {
+                .type = EVENT_INCOMING_CAN,
+                .can = {
+                    .id = 0x7e8,
+                    .dlc = 8,
+                    .data = { 0xEF, 0x23, 0x4F, 0x50, 0x51, 0x24, 0x24, 0x24 },
+                    .frame = (void*)(uintptr_t)0xDEADBEEF,
+                },
+            },
+            {
+                .type = EVENT_SHUTDOWN,
+            },
+        };
+        static int index = 0;
+        assert(index < (sizeof(events) / sizeof(events[0])));
+        memcpy(evt, &events[index++], sizeof(*evt));
+    }
+
+    void write_frame(uint32_t id, uint8_t dlc, const uint8_t* data) {
+        static int count = 0;
+        assert(count < 1);
+        count++;
+        assert(id == 0x7E0);
+        assert(dlc == 8);
+        assert(data[0] == 0xFE);
+        assert(data[1] == 0x30);
+        assert(data[2] == 0);
+        assert(data[3] == 0);
+        assert(data[4] == 0x42);
+        assert(data[5] == 0x42);
+        assert(data[6] == 0x42);
+        assert(data[7] == 0x42);
+    }
+
+    void read_message_cb(const uint8_t* data, size_t size) {
+        static int count = 0;
+        assert(count < 1);
+        count++;
+        assert(size <= 256);
+        memcpy(read_data, data, size);
+        read_size = size;
+    }
+
+    isotp_event_loop(get_next_event, no_unmatched_frame, write_frame, read_message_cb);
+    assert(read_size == 25);
+    uint8_t expected_data[] = "\x00\x00\x07\xE8\xEF\x49\x02\x01"
+                              "ABCDEFGHIJKLMNOPQ";
+    assert(!memcmp(read_data, expected_data, 25));
+}
+
+static void test_read_multi_normal_nopad(void) {
+    uint8_t read_data[256] = { 0 };
+    size_t read_size = 0;
+
+    void get_next_event(struct isotp_event* evt) {
+        static const struct isotp_event events[] = {
+            {
+                .type = EVENT_RECONFIGURE_PAIRS,
+                .pairs = {
+                    .size = 12,
+                    .data = { 0x00, 0x00, 0x07, 0xE0, 0x00, 0xCC, 0x00, 0x00, 0x07, 0xE8, 0x00, 0xCC },
+                },
+            },
+            {
+                .type = EVENT_INCOMING_CAN,
+                .can = {
+                    .id = 0x7e8,
+                    .dlc = 8,
+                    .data = { 0x10, 0x14, 0x49, 0x02, 0x01, 0x41, 0x42, 0x43 },
+                    .frame = (void*)(uintptr_t)0xDEADBEEF,
+                },
+            },
+            {
+                .type = EVENT_INCOMING_CAN,
+                .can = {
+                    .id = 0x7e8,
+                    .dlc = 8,
+                    .data = { 0x21, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A },
+                    .frame = (void*)(uintptr_t)0xDEADBEEF,
+                },
+            },
+            {
+                .type = EVENT_INCOMING_CAN,
+                .can = {
+                    .id = 0x7e8,
+                    .dlc = 8,
+                    .data = { 0x22, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F, 0x50, 0x51 },
+                    .frame = (void*)(uintptr_t)0xDEADBEEF,
+                },
+            },
+            {
+                .type = EVENT_SHUTDOWN,
+            },
+        };
+        static int index = 0;
+        assert(index < (sizeof(events) / sizeof(events[0])));
+        memcpy(evt, &events[index++], sizeof(*evt));
+    }
+
+    void write_frame(uint32_t id, uint8_t dlc, const uint8_t* data) {
+        static int count = 0;
+        assert(count < 1);
+        count++;
+        assert(id == 0x7E0);
+        assert(dlc == 3);
+        assert(data[0] == 0x30);
+        assert(data[1] == 0);
+        assert(data[2] == 0);
+    }
+
+    void read_message_cb(const uint8_t* data, size_t size) {
+        static int count = 0;
+        assert(count < 1);
+        count++;
+        assert(size <= 256);
+        memcpy(read_data, data, size);
+        read_size = size;
+    }
+
+    isotp_event_loop(get_next_event, no_unmatched_frame, write_frame, read_message_cb);
+    assert(read_size == 24);
+    uint8_t expected_data[] = "\x00\x00\x07\xE8\x49\x02\x01"
+                              "ABCDEFGHIJKLMNOPQ";
+    assert(!memcmp(read_data, expected_data, 24));
+}
+
+static void test_read_multi_extended_nopad(void) {
+    uint8_t read_data[256] = { 0 };
+    size_t read_size = 0;
+
+    void get_next_event(struct isotp_event* evt) {
+        static const struct isotp_event events[] = {
+            {
+                .type = EVENT_RECONFIGURE_PAIRS,
+                .pairs = {
+                    .size = 12,
+                    .data = { 0x40, 0x00, 0x07, 0xE0, 0xFE, 0x42, 0x40, 0x00, 0x07, 0xE8, 0xEF, 0x24 },
+                },
+            },
+            {
+                .type = EVENT_INCOMING_CAN,
+                .can = {
+                    .id = 0x7e8,
+                    .dlc = 8,
+                    .data = { 0xEF, 0x10, 0x14, 0x49, 0x02, 0x01, 0x41, 0x42 },
+                    .frame = (void*)(uintptr_t)0xDEADBEEF,
+                },
+            },
+            {
+                .type = EVENT_INCOMING_CAN,
+                .can = {
+                    .id = 0x7e8,
+                    .dlc = 8,
+                    .data = { 0xEF, 0x21, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48 },
+                    .frame = (void*)(uintptr_t)0xDEADBEEF,
+                },
+            },
+            {
+                .type = EVENT_INCOMING_CAN,
+                .can = {
+                    .id = 0x7e8,
+                    .dlc = 8,
+                    .data = { 0xEF, 0x22, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E },
+                    .frame = (void*)(uintptr_t)0xDEADBEEF,
+                },
+            },
+            {
+                .type = EVENT_INCOMING_CAN,
+                .can = {
+                    .id = 0x7e8,
+                    .dlc = 5,
+                    .data = { 0xEF, 0x23, 0x4F, 0x50, 0x51 },
+                    .frame = (void*)(uintptr_t)0xDEADBEEF,
+                },
+            },
+            {
+                .type = EVENT_SHUTDOWN,
+            },
+        };
+        static int index = 0;
+        assert(index < (sizeof(events) / sizeof(events[0])));
+        memcpy(evt, &events[index++], sizeof(*evt));
+    }
+
+    void write_frame(uint32_t id, uint8_t dlc, const uint8_t* data) {
+        static int count = 0;
+        assert(count < 1);
+        count++;
+        assert(id == 0x7E0);
+        assert(dlc == 4);
+        assert(data[0] == 0xFE);
+        assert(data[1] == 0x30);
+        assert(data[2] == 0);
+        assert(data[3] == 0);
+    }
+
+    void read_message_cb(const uint8_t* data, size_t size) {
+        static int count = 0;
+        assert(count < 1);
+        count++;
+        assert(size <= 256);
+        memcpy(read_data, data, size);
+        read_size = size;
+    }
+
+    isotp_event_loop(get_next_event, no_unmatched_frame, write_frame, read_message_cb);
+    assert(read_size == 25);
+    uint8_t expected_data[] = "\x00\x00\x07\xE8\xEF\x49\x02\x01"
+                              "ABCDEFGHIJKLMNOPQ";
+    assert(!memcmp(read_data, expected_data, 25));
 }
 
 void test_send_sf_normal(void) {
@@ -378,7 +630,10 @@ void test_send_sf_extended_nopad(void) {
 int main(int argc, char** argv) {
     test_send_sf_no_configure();
     test_read_unmatched();
-    test_read_multi();
+    test_read_multi_normal();
+    test_read_multi_extended();
+    test_read_multi_normal_nopad();
+    test_read_multi_extended_nopad();
     test_send_sf_normal();
     test_send_sf_normal_nopad();
     test_send_sf_extended();
