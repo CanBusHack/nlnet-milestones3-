2cefcc2b8b50d256bd734769a206c1bb183cd52c
Isaac Tepper <isaac@canbushack.com>
2024-09-13 10:37:41 -0400

    Log all incoming and outgoing CAN frames

--- components/omni_hello/hello.c
+++ components/omni_hello/hello.c
@@ -1,8 +1,9 @@
 #include <assert.h>
 #include <esp_err.h>
 #include <esp_log.h>
-#include <stdint.h>
+#include <inttypes.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <string.h>
 
 #include <driver/gpio.h>
@@ -31,6 +32,10 @@ static StackType_t isotp_task_stack[4096];
 static StaticTask_t isotp_task_buffer;
 static TaskHandle_t isotp_task_handle;
 
+static StackType_t isotp_read_task_stack[4096];
+static StaticTask_t isotp_read_task_buffer;
+static TaskHandle_t isotp_read_task_handle;
+
 static uint8_t isotp_event_queue_storage[sizeof(struct isotp_event) * 4];
 static StaticQueue_t isotp_event_queue_buffer;
 static QueueHandle_t isotp_event_queue_handle;
@@ -39,7 +44,10 @@ static uint8_t isotp_unmatched_frame_queue_storage[sizeof(twai_message_t) * 4];
 static StaticQueue_t isotp_unmatched_frame_queue_buffer;
 static QueueHandle_t isotp_unmatched_frame_queue_handle;
 
-struct isotp_msg { size_t size; uint8_t data[256]; };
+struct isotp_msg {
+    size_t size;
+    uint8_t data[256];
+};
 static uint8_t isotp_msg_queue_storage[sizeof(struct isotp_msg) * 4];
 static StaticQueue_t isotp_msg_queue_buffer;
 static QueueHandle_t isotp_msg_queue_handle;
@@ -64,8 +72,35 @@ static void write_frame(uint32_t id, uint8_t dlc, const uint8_t* data) {
         .data_length_code = dlc,
     };
     memcpy(msg.data, data, (dlc > 8) ? 8 : dlc);
-    // NOTE: if the TX queue is full the frame will be dropped!
-    twai_transmit(&msg, 0);
+    if (msg.extd) {
+        ESP_LOGI(tag, "about to write frame: ID=%08" PRIX32 ", DLC=%X, DATA=%02X%02X%02X%02X%02X%02X%02X%02X, EXTD=T", msg.identifier, msg.data_length_code, msg.data[0], msg.data[1], msg.data[2], msg.data[3], msg.data[4], msg.data[5], msg.data[6], msg.data[7]);
+    } else {
+        ESP_LOGI(tag, "about to write frame: ID=%03" PRIX32 ", DLC=%X, DATA=%02X%02X%02X%02X%02X%02X%02X%02X, EXTD=F", msg.identifier, msg.data_length_code, msg.data[0], msg.data[1], msg.data[2], msg.data[3], msg.data[4], msg.data[5], msg.data[6], msg.data[7]);
+    }
+    esp_err_t result = twai_transmit(&msg, 0);
+    switch (result) {
+    case ESP_OK:
+        ESP_LOGI(tag, "frame write successful");
+        break;
+    case ESP_ERR_INVALID_ARG:
+        ESP_LOGE(tag, "frame write failed: invalid argument");
+        break;
+    case ESP_ERR_TIMEOUT:
+        ESP_LOGE(tag, "frame write failed: timeout");
+        break;
+    case ESP_FAIL:
+        ESP_LOGE(tag, "frame write failed: TX queue disabled and another message is currently transmitting");
+        break;
+    case ESP_ERR_INVALID_STATE:
+        ESP_LOGE(tag, "frame write failed: driver is not running or installed");
+        break;
+    case ESP_ERR_NOT_SUPPORTED:
+        ESP_LOGE(tag, "frame write failed: listen only mode does not support transmissions");
+        break;
+    default:
+        ESP_LOGE(tag, "frame write failed: unknown error");
+        break;
+    }
 }
 
 static void read_message_cb(const uint8_t* data, size_t size) {
@@ -81,6 +116,49 @@ static void read_message_cb(const uint8_t* data, size_t size) {
 
 static void isotp_task(void* ptr) {
     isotp_event_loop(get_next_event, unmatched_frame, write_frame, read_message_cb);
+    vTaskDelete(NULL);
+}
+
+static void isotp_read_task(void* ptr) {
+    for (;;) {
+        twai_message_t msg;
+        ESP_LOGI(tag, "waiting for next incoming frame...");
+        esp_err_t result = twai_receive(&msg, portMAX_DELAY);
+        switch (result) {
+        case ESP_OK:
+            if (msg.extd) {
+                ESP_LOGI(tag, "incoming frame received: ID=%08" PRIX32 ", DLC=%X, DATA=%02X%02X%02X%02X%02X%02X%02X%02X, EXTD=T", msg.identifier, msg.data_length_code, msg.data[0], msg.data[1], msg.data[2], msg.data[3], msg.data[4], msg.data[5], msg.data[6], msg.data[7]);
+            } else {
+                ESP_LOGI(tag, "incoming frame received: ID=%03" PRIX32 ", DLC=%X, DATA=%02X%02X%02X%02X%02X%02X%02X%02X, EXTD=F", msg.identifier, msg.data_length_code, msg.data[0], msg.data[1], msg.data[2], msg.data[3], msg.data[4], msg.data[5], msg.data[6], msg.data[7]);
+            }
+            struct isotp_event event = {
+                .type = EVENT_INCOMING_CAN,
+                .can = {
+                    .id = msg.identifier | (msg.extd << 31),
+                    .dlc = msg.data_length_code,
+                },
+            };
+            memcpy(event.can.data, msg.data, (msg.data_length_code < 8) ? msg.data_length_code : 8);
+            if (xQueueSend(isotp_event_queue_handle, &event, 0) == pdTRUE) {
+                ESP_LOGI(tag, "queued incoming frame event");
+            } else {
+                ESP_LOGE(tag, "event queue error (full?)");
+            }
+            break;
+        case ESP_ERR_TIMEOUT:
+            ESP_LOGE(tag, "frame read failed: timeout");
+            break;
+        case ESP_ERR_INVALID_ARG:
+            ESP_LOGE(tag, "frame read failed: invalid argument");
+            break;
+        case ESP_ERR_INVALID_STATE:
+            ESP_LOGE(tag, "frame read failed: driver is not running or installed");
+            break;
+        default:
+            ESP_LOGE(tag, "frame read failed: unknown error");
+            break;
+        }
+    }
     vTaskDelete(NULL);
 }
 
@@ -315,4 +393,5 @@ void omni_hello_main(void) {
     isotp_unmatched_frame_queue_handle = xQueueCreateStatic(4, sizeof(twai_message_t), isotp_unmatched_frame_queue_storage, &isotp_unmatched_frame_queue_buffer);
     isotp_msg_queue_handle = xQueueCreateStatic(4, sizeof(struct isotp_msg), isotp_msg_queue_storage, &isotp_msg_queue_buffer);
     isotp_task_handle = xTaskCreateStatic(isotp_task, "isotp_task", sizeof(isotp_task_stack) / sizeof(isotp_task_stack[0]), NULL, 5, isotp_task_stack, &isotp_task_buffer);
+    isotp_read_task_handle = xTaskCreateStatic(isotp_read_task, "isotp_read_task", sizeof(isotp_read_task_stack) / sizeof(isotp_read_task_stack[0]), NULL, 5, isotp_read_task_stack, &isotp_read_task_buffer);
 }
