1ef3d33aa66f19eb659b56292e218f0f8879823d
Isaac Tepper <isaac@canbushack.com>
2024-09-30 16:23:25 -0400

    Added test for ISO-TP reading

--- test/main/can/isotp/read.c
+++ test/main/can/isotp/read.c
@@ -19,22 +19,26 @@ static const ble_uuid128_t hello_svc = BLE_UUID128_INIT(0x46, 0x9a, 0x1b, 0xa2, 
 static const ble_uuid128_t isotp_pairs_chr = BLE_UUID128_INIT(0x39, 0x9d, 0x8e, 0x6a, 0x12, 0x6e, 0x8b, 0xb1, 0x57, 0x4d, 0xd7, 0xdf, 0x1d, 0x80, 0x31, 0x7e);
 static const ble_uuid128_t isotp_msg_chr = BLE_UUID128_INIT(0x1e, 0x9a, 0x7a, 0x3f, 0x3f, 0x9e, 0x6f, 0x87, 0x3a, 0x42, 0x2b, 0xb9, 0xe1, 0xd4, 0x13, 0x28);
 
-static const uint8_t pairs[] = { 0x20, 0x00, 0x07, 0xE8, 0x00, 0xCC, 0x20, 0x00, 0x07, 0xE0, 0x00, 0xCC };
-static const uint8_t message[] = { 0x00, 0x00, 0x07, 0xE8, 0x49, 0x02, 0x01, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F, 0x50, 0x51 };
+static const uint8_t pairs[] = { 0x20, 0x00, 0x07, 0xE0, 0x00, 0xCC, 0x20, 0x00, 0x07, 0xE8, 0x00, 0xCC };
 static const twai_message_t flow_control = { .identifier = 0x7E0, .data_length_code = 8, .data = { 0x30, 0x00, 0x00, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC } };
-static const twai_message_t expected[] = {
+static twai_message_t flow_control_r = { 0 };
+static const twai_message_t messages[] = {
     { .identifier = 0x7E8, .data_length_code = 8, .data = { 0x10, 0x14, 0x49, 0x02, 0x01, 0x41, 0x42, 0x43 } },
     { .identifier = 0x7E8, .data_length_code = 8, .data = { 0x21, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A } },
     { .identifier = 0x7E8, .data_length_code = 8, .data = { 0x22, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F, 0x50, 0x51 } },
 };
-static twai_message_t actual[3] = { 0 };
+static const uint8_t expected[] = { 0x00, 0x00, 0x07, 0xE8, 0x49, 0x02, 0x01, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F, 0x50, 0x51 };
+static uint8_t actual[sizeof(expected)] = { 0 };
 static jmp_buf out;
 
-static int write2_cb(uint16_t conn_handle, const struct ble_gatt_error* error, struct ble_gatt_attr* attr, void* arg) {
+static int read_cb(uint16_t conn_handle, const struct ble_gatt_error* error, struct ble_gatt_attr* attr, void* arg) {
     static int count = 0;
     TEST_ASSERT_EQUAL(0, count);
     count++;
     TEST_ASSERT_EQUAL_HEX(0, error->status);
+    uint16_t len;
+    TEST_ASSERT_EQUAL_HEX(0, ble_hs_mbuf_to_flat(attr->om, actual, sizeof(actual), &len));
+    TEST_ASSERT_EQUAL(sizeof(expected), len);
     longjmp(out, 1);
     return 0;
 }
@@ -48,7 +52,13 @@ static int chr2_cb(uint16_t conn_handle, const struct ble_gatt_error* error, con
     count++;
     TEST_ASSERT_EQUAL_HEX(0, error->status);
     TEST_ASSERT(chr);
-    TEST_ASSERT_EQUAL_HEX(0, ble_gattc_write_flat(conn_handle, chr->val_handle, &message, sizeof(message), write2_cb, NULL));
+    TEST_ASSERT_EQUAL(ESP_OK, twai_transmit(messages, pdMS_TO_TICKS(30000)));
+    TEST_ASSERT_EQUAL(ESP_OK, twai_receive(&flow_control_r, pdMS_TO_TICKS(30000)));
+    TEST_ASSERT_EQUAL_MEMORY(&flow_control, &flow_control_r, sizeof(flow_control));
+    TEST_ASSERT_EQUAL(ESP_OK, twai_transmit(messages + 1, pdMS_TO_TICKS(30000)));
+    TEST_ASSERT_EQUAL(ESP_OK, twai_transmit(messages + 2, pdMS_TO_TICKS(30000)));
+    vTaskDelay(pdMS_TO_TICKS(1000)); // some delay for processing
+    TEST_ASSERT_EQUAL_HEX(0, ble_gattc_read(conn_handle, chr->val_handle, read_cb, NULL));
     return 0;
 }
 
@@ -177,8 +187,7 @@ static void sync_cb(void) {
     scan();
 }
 
-TEST_CASE("CAN ISO-TP endpoint - write multi", "[ble][can][isotp]") {
-    TEST_IGNORE_MESSAGE("multi-frame writes are not yet implemented");
+TEST_CASE("CAN ISO-TP endpoint - read", "[ble][can][isotp]") {
     memset(actual, 0, sizeof(actual));
     start_handle = 0;
     end_handle = 0;
@@ -193,9 +202,5 @@ TEST_CASE("CAN ISO-TP endpoint - write multi", "[ble][can][isotp]") {
     if (!setjmp(out)) {
         nimble_port_run();
     }
-    TEST_ASSERT_EQUAL(ESP_OK, twai_receive(actual, pdMS_TO_TICKS(30000)));
-    TEST_ASSERT_EQUAL(ESP_OK, twai_transmit(&flow_control, pdMS_TO_TICKS(30000)));
-    TEST_ASSERT_EQUAL(ESP_OK, twai_receive(actual + 1, pdMS_TO_TICKS(30000)));
-    TEST_ASSERT_EQUAL(ESP_OK, twai_receive(actual + 2, pdMS_TO_TICKS(30000)));
     TEST_ASSERT_EQUAL_MEMORY(&expected, &actual, sizeof(expected));
 }
